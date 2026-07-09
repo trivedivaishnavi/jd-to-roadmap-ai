535728bf9d4bc1d1794f462f8b6d273bcafc2110
Vaishnavi Trivedi <tvaishnavi3012@gmail.com>
2026-07-09 10:44:16 +0530

    Day 2: added form submission and Jinja2 rendering, fixed template folder structure

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    jd_text = request.form['jd_text']
+    return render_template('result.html', jd_text=jd_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
